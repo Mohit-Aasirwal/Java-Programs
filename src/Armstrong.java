dd14bef87803feab14e6480550f6c05b4f7175a6
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-08-04 17:13:41 +0530

    4 august java loop if else etc

---
+++ src/Armstrong.java
@@ -0,0 +1,26 @@
+//import java.util.Scanner;
+//
+//public class Armstrong {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("lowerLimit: ");
+//        int l = input.nextInt();
+//        System.out.print("upperLimit: ");
+//        int u = input.nextInt();
+//        int sum =0;
+//        int num;
+//        for(num=u;num>=l;num--){
+//            for(;;){
+//            int a =num/10;
+//            num=num%10;
+//            int b=a*a*a
+//            sum+=b;
+//            for(;;)
+//            {
+//             if()
+//            }
+//            }
+//            }
+//        }
+//    }
+//}
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
-
-public class Palindrome {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String text = input.next();
-
-
-    }
-}
+//import java.util.Scanner;
+////after arrays
+//public class Palindrome {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//         String text = input.next();
+//        System.out.println(
+//          text.getChars(0, args.length););
+//
+//    }
+//}
