dad6887f5c8e00792a106209e8868a84d7e93eae
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-08-05 17:53:17 +0530

    evening commit

---
+++ src/SumofInfiniteNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumofInfiniteNumbers {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        int sum =0;
+        while(){
+             a = scan.nextInt();
+            sum +=a;
+            System.out.print("Sum of Entered Numbers are: "+sum);
+        }
+
+
+    }
+}
