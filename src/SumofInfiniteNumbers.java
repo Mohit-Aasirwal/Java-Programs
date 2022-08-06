a6124081ae39bba9b5f4b7c2c4b11b5179f53075
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-08-06 09:55:42 +0530

    morning commit

--- src/SumofInfiniteNumbers.java
+++ src/SumofInfiniteNumbers.java
@@ -4,11 +4,14 @@ public class SumofInfiniteNumbers {
     public static void main(String[] args) {
         Scanner scan =new Scanner(System.in);
         int sum =0;
-        while(){
-             a = scan.nextInt();
+        while(true){
+             int a = scan.nextInt();
             sum +=a;
-            System.out.print("Sum of Entered Numbers are: "+sum);
+            if(a==0){
+                break;
+            }
         }
+        System.out.print("Sum of Entered Numbers are: "+sum);
 
 
     }
