396b3a66a1d57fd47d38c0173854278cb47691f8
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-08-14 10:29:00 +0530

    new programs

--- src/Armstrong.java
+++ src/Armstrong.java
@@ -1,26 +1,25 @@
-//import java.util.Scanner;
-//
-//public class Armstrong {
-//    public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("lowerLimit: ");
-//        int l = input.nextInt();
-//        System.out.print("upperLimit: ");
-//        int u = input.nextInt();
-//        int sum =0;
-//        int num;
-//        for(num=u;num>=l;num--){
-//            for(;;){
-//            int a =num/10;
-//            num=num%10;
-//            int b=a*a*a
-//            sum+=b;
-//            for(;;)
-//            {
-//             if()
-//            }
-//            }
-//            }
-//        }
-//    }
-//}
+import java.util.Scanner;
+import java.lang.Math;
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("lowerLimit: ");
+        int l = input.nextInt();
+        System.out.print("upperLimit: ");
+        int u = input.nextInt();
+        int sum = 0;
+        for (int i = u; i >= l; i--) {
+            while (i > 0) {
+                int a = i % 10;
+                int b = (int) Math.pow(a, 3);
+                sum += b;
+                i = i / 10;
+
+                }
+            if (sum == i) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
