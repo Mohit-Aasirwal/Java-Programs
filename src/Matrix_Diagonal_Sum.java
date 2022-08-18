69417889b3d09f08d4198310a7d0328af37682e7
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-08-18 20:44:21 +0530

    night commit 18 august

--- src/Matrix_Diagonal_Sum.java
+++ src/Matrix_Diagonal_Sum.java
@@ -1,19 +1,24 @@
-//public class Matrix_Diagonal_Sum {
-//    public static void main(String[] args) {
-//        int[][] mat ={
-//                {1,2,3},
-//                {4,5,6},
-//                {7,8,9}
-//        };
-//        int sum =0;
-//        for(int i =0; i<mat.length; i++){
-//            for (int j=0; j<mat[i].length;j++){
-//                if(i==j || j==mat[i].length-i){
-//                    sum+=mat[i][j];
-//                }
-//            }
-//        }
-//        System.out.println(sum);
-//
-//    }
-//}
+//not solved
+public class Matrix_Diagonal_Sum {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int sum = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+               int repeating = mat[i].length-1/2;
+                if (mat.length % 2 == 0 && (i == j || j == mat[i].length - i)) {
+                    sum += mat[i][j];
+                }else if(i == j || j == mat[i].length - i){
+                    sum+=mat[i][j];
+                }
+            }
+            System.out.println(sum);
+
+        }
+    }
+}
